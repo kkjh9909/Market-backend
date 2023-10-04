b820a642ec6dc333dc8d2b49d404a489171e8efd
kkjh9909 <kkjh990957@gmail.com>
2023-10-04 16:02:26 +0900

    파일 위치 변경

--- src/main/java/com/example/marketbackend/dto/like/response/LikeAddResponse.java
+++ src/main/java/com/example/marketbackend/dto/like/response/LikeAddResponse.java
@@ -1,4 +1,4 @@
-package com.example.marketbackend.dto.like;
+package com.example.marketbackend.dto.like.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
