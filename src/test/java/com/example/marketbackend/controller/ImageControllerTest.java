26c73042e6a9a63f41aecb8eb60aef90367365d1
kkjh9909 <kkjh990957@gmail.com>
2023-10-16 23:34:31 +0900

    Image Upload Api 문서 작성

---
+++ src/test/java/com/example/marketbackend/controller/ImageControllerTest.java
@@ -0,0 +1,80 @@
+package com.example.marketbackend.controller;
+
+import com.example.marketbackend.dto.Response;
+import com.example.marketbackend.dto.post.request.ProductPostWriteRequest;
+import com.example.marketbackend.dto.user.request.UserSignInRequest;
+import com.example.marketbackend.dto.user.request.UserSignUpRequest;
+import com.example.marketbackend.dto.user.response.UserSignInResponse;
+import com.example.marketbackend.entity.Chat;
+import com.example.marketbackend.entity.ChatRoom;
+import com.example.marketbackend.entity.ProductPost;
+import com.example.marketbackend.repository.*;
+import com.example.marketbackend.service.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+class ImageControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void uploadImage() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "hello.jpg", MediaType.IMAGE_JPEG_VALUE,  "Hello, World!".getBytes());
+
+        mockMvc.perform(
+                        multipart("/images/upload").file(file)
+                )
+                .andExpect(status().isOk())
+                .andDo(
+                        document("image-upload",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestParts(
+                                        partWithName("file").description("업로드할 파일")
+                                ),
+                                responseFields(
+                                        fieldWithPath("image_url").description("이미지 파일 경로")
+                                )
+                        )
+                );
+    }
+}
