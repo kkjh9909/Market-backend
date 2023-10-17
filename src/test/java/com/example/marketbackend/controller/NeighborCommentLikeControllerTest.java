547499daa234897d4d71554beb5265de14329638
kkjh9909 <kkjh990957@gmail.com>
2023-10-17 19:37:48 +0900

    Neighbor Post Comment Like Api 문서 작성

---
+++ src/test/java/com/example/marketbackend/controller/NeighborCommentLikeControllerTest.java
@@ -0,0 +1,170 @@
+package com.example.marketbackend.controller;
+
+import com.example.marketbackend.dto.Response;
+import com.example.marketbackend.dto.neighbor.comment.request.NeighborCommentWriteRequest;
+import com.example.marketbackend.dto.neighbor.post.request.NeighborPostWriteRequest;
+import com.example.marketbackend.dto.post.request.ProductPostWriteRequest;
+import com.example.marketbackend.dto.user.request.UserSignInRequest;
+import com.example.marketbackend.dto.user.request.UserSignUpRequest;
+import com.example.marketbackend.dto.user.response.UserSignInResponse;
+import com.example.marketbackend.entity.ProductPost;
+import com.example.marketbackend.service.NeighborCommentService;
+import com.example.marketbackend.service.NeighborPostService;
+import com.example.marketbackend.service.ProductPostService;
+import com.example.marketbackend.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+class NeighborCommentLikeControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private NeighborPostService neighborPostService;
+
+    @Autowired
+    private NeighborCommentService neighborCommentService;
+
+    public String getToken() {
+        signup();
+
+        Response response = userService.signIn(new UserSignInRequest("userId", "password"));
+
+        UserSignInResponse result = (UserSignInResponse) response.getResult();
+
+        return result.getAccessToken();
+    }
+
+    public void signup() {
+        UserSignUpRequest request = new UserSignUpRequest("userId", "password", "username", "nickname", "profileImage", "address");
+
+        userService.signUp(request);
+    }
+
+    public void makePost() {
+        UserDetailsService userDetailsService = Mockito.mock(UserDetailsService.class);
+
+        UserDetails userDetails = User.withUsername("1")
+                .password("encodedPassword")
+                .roles("USER")
+                .build();
+
+        Mockito.when(userDetailsService.loadUserByUsername("userId")).thenReturn(userDetails);
+
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
+
+        NeighborPostWriteRequest request = new NeighborPostWriteRequest("title",  "content", "category", new String[]{"imageUrl1", "imageUrl2"});
+
+        neighborPostService.write(request);
+    }
+
+    @Test
+    void addLike() throws Exception {
+        String token = getToken();
+
+        makePost();
+
+        NeighborCommentWriteRequest request = new NeighborCommentWriteRequest("content", null);
+
+        neighborCommentService.write(2L, request);
+
+        mockMvc.perform(
+                        post("/api/neighbor/comment/like/{commentId}", 5L)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + token)
+                )
+                .andExpect(status().isOk())
+                .andDo(
+                        MockMvcRestDocumentation.document("neighbor-comment-like-add",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestHeaders(
+                                        headerWithName("Authorization").description("인증 토큰")
+                                ),
+                                pathParameters(
+                                        parameterWithName("commentId").description("댓글 ID")
+                                ),
+                                responseFields(
+                                        fieldWithPath("message").description("응답 메시지"),
+                                        subsectionWithPath("result").description("응답 결과"),
+                                        subsectionWithPath("result.like_count").description("좋아요 개수")
+                                )
+                        )
+                );
+    }
+
+    @Test
+    void deleteLike() throws Exception {
+        String token = getToken();
+
+        makePost();
+
+        NeighborCommentWriteRequest request = new NeighborCommentWriteRequest("content", null);
+
+        neighborCommentService.write(2L, request);
+
+        mockMvc.perform(
+                        delete("/api/neighbor/comment/like/{commentId}", 5L)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + token)
+                )
+                .andExpect(status().isOk())
+                .andDo(
+                        MockMvcRestDocumentation.document("neighbor-comment-like-delete",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestHeaders(
+                                        headerWithName("Authorization").description("인증 토큰")
+                                ),
+                                pathParameters(
+                                        parameterWithName("commentId").description("댓글 ID")
+                                ),
+                                responseFields(
+                                        fieldWithPath("message").description("응답 메시지"),
+                                        subsectionWithPath("result").description("응답 결과"),
+                                        subsectionWithPath("result.like_count").description("좋아요 개수")
+                                )
+                        )
+                );
+    }
+}
